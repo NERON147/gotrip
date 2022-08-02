70668254eec3d5419f329279b63a1ab4f6c39df1
NERON147 <bezzubenkomaxim@gmail.com>
2022-08-03 01:41:15 +0300

    update

--- js/main.js
+++ js/main.js
@@ -13,11 +13,33 @@ var swiper = new Swiper(".mySwiper", {
     speed: 600,
     coverflowEffect: {
       rotate: 50,
-      stretch: 30,
+      stretch: 100,
       depth: 200,
-      modifier: 1,
+      modifier: 0.8,
       slideShadows: false,
 
     },
    
   });
+
+  ScrollReveal().reveal('.nav2', {  distance: '60px', origin: 'top', delay: 100});
+  ScrollReveal().reveal('.headline', {  distance: '60px', origin: 'left', delay: 100});
+  ScrollReveal().reveal('.headline2', {  distance: '60px', origin: 'left',  delay: 300});
+  ScrollReveal().reveal('.headline3', {  distance: '60px', origin: 'left',  delay: 500 });
+  ScrollReveal().reveal('.destinations__title2', {  distance: '60px', origin: 'left',delay: 100});
+  ScrollReveal().reveal('.destinations__text2', {  distance: '60px', origin: 'right', delay: 300});
+  ScrollReveal().reveal('.destinations__slider2', {  distance: '60px', origin: 'right', delay: 800});
+  ScrollReveal().reveal('.customers__img2', {  distance: '60px', origin: 'left', delay: 800});
+  ScrollReveal().reveal('.customers__title2', {  distance: '60px', origin: 'right', delay: 1000});
+  ScrollReveal().reveal('.review2', {  distance: '60px', origin: 'right', delay: 1300});
+  ScrollReveal().reveal('.header__btn2', {  distance: '60px', origin: 'right', delay: 800,
+  rotate: {
+    x: 100,
+    y: 0,
+    z: 100,
+}});
+
+
+
+
+ 
