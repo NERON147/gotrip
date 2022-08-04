09e020c69a613d19421caf4829d0f4ba22c206cd
NERON147 <bezzubenkomaxim@gmail.com>
2022-08-04 15:14:35 +0300

    up

--- js/main.js
+++ js/main.js
@@ -52,6 +52,17 @@ var swiper = new Swiper(".mySwiper", {
 }});
 
 
+// Бургер
+const nav = document.querySelector('.nav')
+const navBtn = document.querySelector('.nav__toggle')
+const menuIcon = document.querySelector('.menu-icon')
+
+navBtn.onclick = function(){
+  nav.classList.toggle('nav--mobile');
+  menuIcon.classList.toggle('menu-icon-active');
+}
+
+
 
 
  
